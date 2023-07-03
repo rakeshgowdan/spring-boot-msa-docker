4bd5f372beaa873e5b565659b0db9ea9abd48e46
Rakesh Gowda <rakeshgowda3101@gmail.com>
2023-07-03 14:19:56 +0530

    tracing client details like IP,OS & browser from API call

--- src/main/java/com/rakesh/springBootDocker/controller/CourseController.java
+++ src/main/java/com/rakesh/springBootDocker/controller/CourseController.java
@@ -1,8 +1,13 @@
 package com.rakesh.springBootDocker.controller;
 
 import java.net.URI;
+import java.util.Enumeration;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.StringTokenizer;
 
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -21,9 +26,12 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import com.rakesh.springBootDocker.entities.Course;
 import com.rakesh.springBootDocker.service.CourseServiceImple;
 
+import lombok.extern.slf4j.Slf4j;
+
 @CrossOrigin(origins = { "http://localhost:3000", "http://localhost:4200" })
 @RestController
 @RequestMapping("/api/v1")
+@Slf4j
 public class CourseController {
 
 	
@@ -31,10 +39,94 @@ public class CourseController {
 		@Autowired
 		  private CourseServiceImple courseManagementService;
 		
-		@GetMapping("/instructors/{username}/courses")
-		  public List<Course> getAllCourses(@PathVariable String username) {
-		    return courseManagementService.findAll();
-		  }
+	@GetMapping("/instructors/{username}/courses")
+	public List<Course> getAllCourses(@PathVariable String username, HttpServletRequest request) {
+
+		System.err.println(getIPAddress(request));
+		
+		System.err.println(getClientInformation(request));
+
+		return courseManagementService.findAll();
+	}
+
+	/**
+	 * @param request
+	 */
+	public String getClientInformation(HttpServletRequest request) {
+		String browserDetails = request.getHeader("User-Agent");
+		
+		System.err.println("browserDetails " + browserDetails);
+
+		String userAgent = browserDetails;
+		String user = userAgent.toLowerCase();
+
+		String os = "";
+		String browser = "";
+		System.err.println("User Agent for the request is===>" + browserDetails);
+		// =================OS=======================
+		if (userAgent.toLowerCase().indexOf("windows") >= 0) {
+			os = "Windows";
+		} else if (userAgent.toLowerCase().indexOf("mac") >= 0) {
+			os = "Mac";
+		} else if (userAgent.toLowerCase().indexOf("x11") >= 0) {
+			os = "Unix";
+		} else if (userAgent.toLowerCase().indexOf("android") >= 0) {
+			os = "Android";
+		} else if (userAgent.toLowerCase().indexOf("iphone") >= 0) {
+			os = "IPhone";
+		} else {
+			os = "UnKnown, More-Info: " + userAgent;
+		}
+		// ===============Browser===========================
+		if (user.contains("edg")) {
+			log.info("edge");
+			browser = (userAgent.substring(userAgent.indexOf("Edg")).split(" ")[0]).replace("/", "-");
+		} else if (user.contains("msie")) {
+			log.info("msie");
+			String substring = userAgent.substring(userAgent.indexOf("MSIE")).split(";")[0];
+			browser = substring.split(" ")[0].replace("MSIE", "IE") + "-" + substring.split(" ")[1];
+		} else if (user.contains("safari") && user.contains("version")) {
+			log.info("safari");
+			browser = (userAgent.substring(userAgent.indexOf("Safari")).split(" ")[0]).split("/")[0] + "-"
+					+ (userAgent.substring(userAgent.indexOf("Version")).split(" ")[0]).split("/")[1];
+		} else if (user.contains("opr") || user.contains("opera")) {
+			log.info("opera");
+			if (user.contains("opera"))
+				browser = (userAgent.substring(userAgent.indexOf("Opera")).split(" ")[0]).split("/")[0] + "-"
+						+ (userAgent.substring(userAgent.indexOf("Version")).split(" ")[0]).split("/")[1];
+			else if (user.contains("opr"))
+				browser = ((userAgent.substring(userAgent.indexOf("OPR")).split(" ")[0]).replace("/", "-"))
+						.replace("OPR", "Opera");
+		} else if (user.contains("chrome")) {
+			log.info("chrome");
+			browser = (userAgent.substring(userAgent.indexOf("Chrome")).split(" ")[0]).replace("/", "-");
+		} else if (user.contains("firefox")) {
+			log.info("firefox");
+			browser = (userAgent.substring(userAgent.indexOf("Firefox")).split(" ")[0]).replace("/", "-");
+		} else if (user.contains("rv")) {
+			log.info("rv");
+			browser = "IE-" + user.substring(user.indexOf("rv") + 3, user.indexOf(")"));
+		}
+		else {
+			log.info("Browser not defined");
+			browser = "UnKnown, More-Info: " + userAgent;
+		}
+		System.err.println("Operating System======>" + os);
+		System.err.println("Browser Name==========>" + browser);
+		return os+","+browser;
+	}
+
+	/**
+	 * @param request
+	 */
+	public String getIPAddress(HttpServletRequest request) {
+		String ipAddress = request.getHeader("X-FORWARDED-FOR");
+		if (ipAddress == null) {
+			ipAddress = request.getRemoteAddr();
+		}
+		log.info("IP " + ipAddress);
+		return ipAddress;
+	}
 
 		@GetMapping("/instructors/{username}/courses/{id}")
 		  public Course getCourse(@PathVariable String username, @PathVariable long id) {
@@ -79,5 +171,6 @@ public class CourseController {
 		    return ResponseEntity.created(uri).build();
 		  }
 
+		
 	
 }
